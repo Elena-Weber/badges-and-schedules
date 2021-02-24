b96f2bb2386daf46c4c33adfc85ed9c50f185b55
Elena-Weber <crochetmiracles@gmail.com>
2021-02-24 15:25:29 -0500

    commit tree ba3acd75be6e3ae8f43b01a0a5899e1541b4e6b0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+require 'pry'
+
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(name)
+    list = []
+    name.each do |guest|
+    list << "Hello, my name is #{guest}."
+    end
+    return list
+end
+
+def assign_rooms(name)
+    guests = []
+    name.each.with_index(1) do |guest, i|
+    guests << "Hello, #{guest}! You'll be assigned to room #{i}!"
+    end
+    return guests
+end
+
+def printer(name)
+batch_badge_creator(name).each do |guest|
+    puts guest
+    end
+assign_rooms(name).each do |guest|
+    puts guest
+    end
+end
